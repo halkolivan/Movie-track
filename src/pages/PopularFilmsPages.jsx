b23cd0140360a50270be8773fc7562b79f383cff
M3 <mthree.webdev@gmail.com>
2024-05-18 00:18:28 +0300

    fix

--- src/pages/PopularFilmsPages.jsx
+++ src/pages/PopularFilmsPages.jsx
@@ -1,38 +1,67 @@
-import { useEffect } from "react";
-import { NavLink } from "react-router-dom";
-import { getFilmsPopular } from "../store/slices/home";
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { getFilmsPopular } from '../store/slices/home'
+import { useSelector, useDispatch } from 'react-redux'
 
 //import style
-import "src/assets/styles/pages/PopularFilmsPages.scss";
+import 'src/assets/styles/pages/PopularFilmsPages.scss'
 
 export default function PopularFilmsPages() {
-  const requestPopularFilms = useSelector((state) => state.home.results);
-  const loading = useSelector((state) => state.home.loading);
-  const dispatch = useDispatch();
+  //Initialization variables
+  const dispatch = useDispatch()
+  const { t, i18n } = useTranslation()
+
+  //Initialization state manager
+
+  const [page, setPage] = useState(1)
+  const loading = useSelector(state => state.home.loading)
+  const requestPopularFilms = useSelector(state => state.home.results)
+
+  //Function for request popular films
   useEffect(() => {
-    dispatch(getFilmsPopular());
-  }, []);
+    dispatch(
+      getFilmsPopular({
+        page: page,
+        language: 'ru-RU',
+      })
+    )
+  }, [page])
 
   return (
-    <div className="main-content">
-      <div className="films-page">
+    <div className='main-content'>
+      <button
+        onClick={() =>
+          setPage(old => {
+            if (old > 1) {
+              return old - 1
+            } else {
+              return old
+            }
+          })
+        }>
+        -
+      </button>
+      <button onClick={() => setPage(old => old + 1)}>+</button>
+      <div className='films-page'>
         {!loading ? (
-          <div className="films-page-content">
+          <div className='films-page-content'>
             {requestPopularFilms ? (
-              requestPopularFilms.map((item) => (
-                <NavLink to={`/detailsFilm/${item.id}`} key={item.id}>
-                  <div className="films-list">
-                    <div className="films-list-image">
+              requestPopularFilms.map(item => (
+                <NavLink
+                  to={`/detailsFilm/${item.id}`}
+                  key={item.id}>
+                  <div className='films-list'>
+                    <div className='films-list-image'>
                       <img
                         src={
-                          "https://image.tmdb.org/t/p/original/" +
+                          'https://image.tmdb.org/t/p/original/' +
                           item.poster_path
                         }
-                        alt=""
+                        alt=''
                       />
                     </div>
-                    <div className="films-list-descript">{item.overview}</div>
+                    <div className='films-list-descript'>{item.overview}</div>
                   </div>
                 </NavLink>
               ))
@@ -44,7 +73,7 @@ export default function PopularFilmsPages() {
           <p>данные не загружены</p>
         )}
       </div>
-      <div className="page-numbers">Нумерация страниц</div>
+      <div className='page-numbers'>Нумерация страниц</div>
     </div>
-  );
+  )
 }
--- src/store/slices/home.js
+++ src/store/slices/home.js
@@ -1,24 +1,47 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios'
+import Request from '../../helpers/request'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
-import axios from "axios";
+// export const getFilmsPopular = createAsyncThunk(
+//   'homeRequest/getFilmsPopular',
+//   async (_, { rejectedWithValue }) => {
+//     try {
+//       const response = await axios.get(
+//         `https://api.themoviedb.org/3/movie/popular?language=ru-RU&page=5`,
+//         {
+//           headers: {
+//             'Content-Type': 'application/json',
+//             Authorization: `Bearer ${
+//               import.meta.env.VITE_APP_API_ACCESS_TOKEN
+//             }`,
+//           },
+//         }
+//       )
+//       return response.data
+//     } catch (error) {
+//       console.log(error)
+//       return rejectedWithValue(error.response.data)
+//     }
+//   }
+// )
 
 export const getFilmsPopular = createAsyncThunk(
-  "homeRequest/getFilmsPopular",
-  async (_, { rejectedWithValue }) => {
+  'homeRequest/getFilmsPopular',
+  async (data, { rejectedWithValue }) => {
     try {
-      const response = await axios.get(
-        `https://api.themoviedb.org/3/movie/popular?language=ru-RU&page=1`
-      );
-      return response.data;
+      const response = await Request().get(
+        `movie/popular?language=${data.language}&page=${data.page}`
+      )
+      return response.data
     } catch (error) {
-      console.log(error);
-      return rejectedWithValue(error.response.data);
+      console.log(error)
+      return rejectedWithValue(error.response.data)
     }
   }
-);
+)
 
 const homeSlice = createSlice({
-  name: "home",
+  name: 'home',
   initialState: {
     loading: true,
     results: undefined,
@@ -26,18 +49,19 @@ const homeSlice = createSlice({
   },
   reducers: {
     setLoading(state, action) {
-      state.loading = action.payload;
+      state.loading = action.payload
     },
   },
-  extraReducers: (builder) => {
-    builder.addCase(getFilmsPopular.fulfilled, (state, { payload }) => {
-      state.results = payload.results;
-      state.loading = false;
-    });
-    builder.addCase(getFilmsPopular.rejected, (state, { payload }) => {
-      state.popularError = payload;
-    });
+  extraReducers: builder => {
+    builder
+      .addCase(getFilmsPopular.fulfilled, (state, { payload }) => {
+        state.results = payload.results
+        state.loading = false
+      })
+      .addCase(getFilmsPopular.rejected, (state, { payload }) => {
+        state.popularError = payload
+      })
   },
-});
-export const { setLoading } = homeSlice.actions;
-export default homeSlice.reducer;
+})
+export const { setLoading } = homeSlice.actions
+export default homeSlice.reducer
