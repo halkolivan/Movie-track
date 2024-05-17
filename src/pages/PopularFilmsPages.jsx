616a8ee51df00f0e154746c5792a54d3e3a3da48
Roman Tomayli <tomayli@bk.ru>
2024-05-17 23:38:45 +0300

    responce fix 5

--- src/pages/PopularFilmsPages.jsx
+++ src/pages/PopularFilmsPages.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import { getFilmsPopular } from "../store/slices/home";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -7,22 +8,40 @@ import "src/assets/styles/pages/PopularFilmsPages.scss";
 
 export default function PopularFilmsPages() {
   const requestPopularFilms = useSelector((state) => state.home.results);
+  const loading = useSelector((state) => state.home.loading);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getFilmsPopular());
   }, []);
-  console.log(requestPopularFilms)
 
   return (
     <div className="main-content">
       <div className="films-page">
-        {requestPopularFilms ? (
-          <div className="films-list">
-            <div className="films-list-image"></div>
-            <div className="films-list-descript"></div>
+        {!loading ? (
+          <div className="films-page-content">
+            {requestPopularFilms ? (
+              requestPopularFilms.map((item) => (
+                <NavLink to={`/detailsFilm/${item.id}`} key={item.id}>
+                  <div className="films-list">
+                    <div className="films-list-image">
+                      <img
+                        src={
+                          "https://image.tmdb.org/t/p/original/" +
+                          item.poster_path
+                        }
+                        alt=""
+                      />
+                    </div>
+                    <div className="films-list-descript">{item.overview}</div>
+                  </div>
+                </NavLink>
+              ))
+            ) : (
+              <p>запрос отсутствует ...</p>
+            )}
           </div>
         ) : (
-          <p>Нет загрузки</p>
+          <p>данные не загружены</p>
         )}
       </div>
       <div className="page-numbers">Нумерация страниц</div>
